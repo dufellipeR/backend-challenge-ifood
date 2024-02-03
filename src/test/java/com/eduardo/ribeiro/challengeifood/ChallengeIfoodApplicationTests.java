6e01f9a7ad2e7ca62749bba96fc7e5a0f9b2e37e
dufelliper <eduardo.fellipe30@gmail.com>
2024-02-03 11:18:48 -0300

    baseline

---
+++ src/test/java/com/eduardo/ribeiro/challengeifood/ChallengeIfoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.ribeiro.challengeifood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeIfoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
